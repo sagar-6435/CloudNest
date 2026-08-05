dd8edf350d53f9cf63013cc91a28ae1717a8ac18
sagar-6435 <sagarnaidu6435@gmail.com>
2026-08-05 11:26:21 +0530

    update cors rules explicitly

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://cloud-nest1.vercel.app', process.env.FRONTEND_URL],
+    credentials: true,
+}));
 app.use(express.json());
 
 // Routes
